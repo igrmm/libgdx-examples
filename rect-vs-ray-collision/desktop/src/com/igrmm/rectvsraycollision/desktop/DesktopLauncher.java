d5667cfd5a226cee2fe84b2d8c62430c16bd6954
igrmm <igrmmdev@gmail.com>
2020-09-25 14:38:31 -0400

    add rect-vs-ray-collision example made with libgdx wizard

---
+++ rect-vs-ray-collision/desktop/src/com/igrmm/rectvsraycollision/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.igrmm.rectvsraycollision.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.igrmm.rectvsraycollision.GdxExample;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxExample(), config);
+	}
+}
